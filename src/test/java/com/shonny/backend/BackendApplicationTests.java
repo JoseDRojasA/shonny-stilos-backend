845f73166fbae8753dc630d3f78c0b138973fc53
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2019-12-01 17:50:13 -0500

    Initial commit

---
+++ src/test/java/com/shonny/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shonny.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
